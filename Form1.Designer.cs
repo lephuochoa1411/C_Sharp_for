9e05f5ccceca94482a185f77acd062696ac3f156
TamNhien <canhthien91@gmail.com>
2024-06-10 13:29:09 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace C_Sharp_for
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btnt1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập Số ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giai Thừa";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(273, 33);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(273, 63);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(100, 20);
+            this.txtkq.TabIndex = 3;
+            // 
+            // btnt1
+            // 
+            this.btnt1.Location = new System.Drawing.Point(282, 89);
+            this.btnt1.Name = "btnt1";
+            this.btnt1.Size = new System.Drawing.Size(75, 25);
+            this.btnt1.TabIndex = 4;
+            this.btnt1.Text = "Tính";
+            this.btnt1.UseVisualStyleBackColor = true;
+            this.btnt1.Click += new System.EventHandler(this.btnt1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnt1);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button btnt1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Sharp_for
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnt1_Click(object sender, EventArgs e)
+        {
+            double so;
+            so = Convert.ToDouble(btnt1.Text);
+            int gt = 1; 
+            for (int i = 0; i < so; i++)
+            {
+                gt = gt * i;//gt*1;
+            }
+            txtkq.Text = gt.ToString();
+        }
+    }
+}
